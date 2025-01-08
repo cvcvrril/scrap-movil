d50228dbbb73cde53ca185d65697dbcdc55773ff
Inés Martínez <132656887+cvcvrril@users.noreply.github.com>
2025-01-08 10:03:31 +0100

    código básico, modificar después

--- src/main.py
+++ src/main.py
@@ -4,4 +4,23 @@ class BlogSpider(scrapy.Spider):
     name = 'kimovil'
     allowed_domains = ["www.kimovil.com"]
     start_urls = ['https://www.kimovil.com/es/']
+
+
+    def parse(self, response):
+        # get all HTML product elements
+        products = response.css(".product")
+        # iterate over the list of products
+        for product in products:
+            # get the two price text nodes (currency + cost) and
+            # contatenate them
+            price_text_elements = product.css(".price *::text").getall()
+            price = "".join(price_text_elements)
+           
+           # return a generator for the scraped item
+            yield {
+                "Url": product.css("a").attrib["href"],
+                "Image": product.css("img").attrib["src"],
+                "Name": product.css("h2::text").get(),  
+                "Price": price,
+            }
     
